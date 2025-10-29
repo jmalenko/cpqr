ac8dfab3258ed253133fe075329f91b6b3ccd231
Jaromir Malenko <jaro@jaro.cz>
2025-10-29 18:38:04 +0100

    Scan - flag to show timing issues, disabled by default

--- scan.js
+++ scan.js
@@ -291,6 +291,8 @@ let queueLength; // Length of the queue in the worker
 let downloaded; // Whether the file has been downloaded
 let contentPrevious; // Content of previous data in QR code
 
+const logTiming = false;
+
 const worker = new Worker('scanWorker.js');
 
 worker.onmessage = function (e) {
@@ -599,7 +601,7 @@ function initStream() {
             // Measure animation speed
             let statsAnimationFrame = measureIntervalAnimationFrame();
             if (statsAnimationFrame.ms !== undefined) {
-                if (isAboveSigma(statsAnimationFrame, 3)) {
+                if (logTiming && isAboveSigma(statsAnimationFrame, 3)) {
                     log(`WARNING: Animation frame variation ${statsAnimationFrame.ms} ms, avg=${statsAnimationFrame.avg.toFixed(1)} ms, stddev=${statsAnimationFrame.stddev.toFixed(1)} ms`);
                 }
                 document.getElementById("scanSpeed").innerText = "Scan speed: avg " + statsAnimationFrame.avg.toFixed(1) + " ms; last " + statsAnimationFrame.ms.toFixed(1) + " ms.";
@@ -659,7 +661,7 @@ function initStream() {
                     inversionAttempts: "dontInvert",
                 });
             });
-            if (isAboveSigma(statsQrRecognition, 3)) {
+            if (logTiming && isAboveSigma(statsQrRecognition, 3)) {
                 log(`WARNING: Recognize QR code variation ${statsQrRecognition.ms} ms, avg=${statsQrRecognition.avg.toFixed(1)} ms, stddev=${statsQrRecognition.stddev.toFixed(1)} ms`);
             }
 
